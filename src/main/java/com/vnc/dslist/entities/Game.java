06863ec0cf9ab7fa2e0c74987cd004b9e6cfeb5c
Vinicius Vieira <vinicius.carneiro>
2023-05-10 13:52:06 -0300

    properties, Game, ORM

---
+++ src/main/java/com/vnc/dslist/entities/Game.java
@@ -0,0 +1,38 @@
+package com.vnc.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private String year;
+    private String genre;
+    private String platforms;
+    private String score;
+    private String imgUri;
+    private String shortDescrition;
+    private String longDescrition;
+
+}
